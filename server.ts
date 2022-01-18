591b1363657f9fd7631da384b964d9dc82b61528
saritChauhan <saritsinghchauhan@gmail.com>
2022-01-18 16:58:42 +0530

    added version

--- server.ts
+++ server.ts
@@ -6,6 +6,10 @@ app.get("/", (req : any , res: any)=>{
     res.send("Hello World!")
 })
 
+app.get("/version", (req : any , res: any)=>{
+    res.send({v:1})
+})
+
 const port = process.env.PORT || 5000
 app.listen(port, ()=>{
     console.log(`server running at ${port}`);
