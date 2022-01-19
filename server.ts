86432964e63aac452380ebf62adf5d52eaef9c34
saritChauhan <saritsinghchauhan@gmail.com>
2022-01-19 09:52:12 +0530

    undo

--- server.ts
+++ server.ts
@@ -7,11 +7,6 @@ app.get("/", (req : any , res: any)=>{
     res.send("Hello World!")
 })
 
-app.get("/version", (req : any , res: any)=>{
-    const version = new Version()
-    res.send(version)
-})
-
 const port = process.env.PORT || 5000
 app.listen(port, ()=>{
     console.log(`server running at ${port}`);
